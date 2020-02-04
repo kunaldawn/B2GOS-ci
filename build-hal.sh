f68db8529e0d22242e899f48312a36524a2fb651
Bingco <ittat@live.com>
2020-02-04 11:05:09 +0800

    Update build-hal.sh

--- build-hal.sh
+++ build-hal.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
 source b2g.env
-cd $ANDROID_ROOT
+cd $ANDROID_ROOT/B2G
 
-
-source build/envsetup.sh
 export USE_CCACHE=1
-lunch full_onyx-eng 
-make -j$(nproc) mkbootimg
-make -j$(nproc) systemimage 
+export B2G_FOTA_FULLIMG_PARTS="/boot:boot.img /system:system.img"
+export B2G_SYSTEM_APPS=1
+export B2G_UPDATE_CHANNEL=default
+export MOZ_TELEMETRY_REPORTING=1
+export MOZ_CRASHREPORTER_NO_REPORT=1 
+export ENABLE_DEFAULT_BOOTANIMATION=true
+export DOGFOOD=0
+export VARIANT=eng
+export DEVICE_DEBUG=1
+export SCREEN_TIMEOUT=0
+export NOFTU=1
+export NO_LOCK_SCREEN=1
+export MOZILLA_OFFICIAL=1
+export GAIA_DISTRIBUTION_DIR=distros/spark
+export GAIA_DEV_PIXELS_PER_PX=2.25
 
-echo "md5sum halium-boot.img and system.img"
-md5sum $ANDROID_ROOT/out/target/product/onyx/boot.img
-md5sum $ANDROID_ROOT/out/target/product/onyx/system.img
+./build.sh -j$(nproc)  gecko-update-fota-fullimg
